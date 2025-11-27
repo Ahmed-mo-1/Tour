157aed6334edc7e13472901e0046a207ab5168c2
ahmed <im.ahmed.mo.1@gmail.com>
2025-11-28 01:52:26 +0200

    phone meta

--- header.php
+++ header.php
@@ -1,9 +1,11 @@
 <html>
   <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+
      <script type="importmap">
       {
         "imports": {
-          "MP_SDK": "https://api.matterport.com/sdk/bootstrap/3.0.0-0-g0517b8d76c/sdk.es6.js?applicationKey=axhr8wp886tecufdcbzanaghc"
+          "MP_SDK": "https://api.matterport.com/sdk/bootstrap/3.0.0-0-g0517b8d76c/sdk.es6.js?applicationKey=crit9r5d4zduc09z4kihmcm7d"
         }
       }
     </script>
